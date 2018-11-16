512d29cae31aaed5a99ec206e60cc1b78478b1dc
Evana <Evana13G@gmail.com>
2018-11-16 17:43:40 -0500

    code is currently broken, because of import issues

--- action_primitive_variation/scripts/APV_server.py
+++ action_primitive_variation/scripts/APV_server.py
@@ -34,13 +34,90 @@ from tf.transformations import *
 import baxter_interface
 
 from action_primitive_variation.srv import *
+# from agent import KnowledgeBase
+
+
+
+
+
+
+
+
+
+
+
+
+class KnowledgeBase(object):
+    def __init__(self, actionPrimitiveNames, actionPrimitiveServices, actionPrimitiveSrvFiles):
+        self.names = actionPrimitiveNames
+        self.services = actionPrimitiveServices
+        self.srvs = actionPrimitiveSrvFiles
+        list_data = []
+        dict_data = {}
+        for i in range(len(self.names)):
+            list_data_point = {}
+            list_data_point['name'] = self.names[i]
+            list_data_point['service'] = self.services[i]
+            list_data_point['srv'] = self.srvs[i]
+            list_data.append(list_data_point)
+            dict_data[self.names[i]] = {}
+            dict_data[self.names[i]]['service'] = self.services[i]
+            dict_data[self.names[i]]['srv'] = self.srvs[i]    
+        self.KB_list = list_data
+        self.KB_dict = dict_data
+
+    def getNames(self):
+        return self.names
+
+    def getServices(self):
+        return self.services
+
+    def getSrvs(self):
+        return self.srvs
+
+    def getService(self, actionName):
+        return self.services[self.names.index(actionName)]
+
+    def getSrv(self):
+        return self.srvs[self.names.index(actionName)]
+
+    def getAPData_list(self):
+        return self.KB_list
+
+    def getAPData_dict(self):
+        return self.KB_dict
+
+    def addAction(self, actionName, service, srv):
+        self.names.append(actionName)
+        self.services.append(service)
+        self.srvs.append(srv)
+        dict_data = {}
+        list_data = {}
+        list_data['name'] = actionName
+        list_data['service'] = service
+        list_data['srv'] = srv
+        dict_data['service'] = service
+        dict_data['srv'] = srv
+        self.KB_list.append(list_data)
+        self.KB_dict[actionName] = dict_data
+
+
+
+
+
+
+
 
 
 actionToVary = None 
 gripper = None
 obj = None
 button = None
-actionPrimitiveKnowledgeBase = {}
+
+AP_names = ['press_button', 'obtain_object']
+AP_services = ['press_button_srv', 'obtain_object_srv']
+AP_srvs = [PressButtonSrv, ObtainObjectSrv]
+KB = KnowledgeBase(AP_names, AP_services, AP_srvs)
 
 
 def handle_APV(req):
@@ -50,8 +127,6 @@ def handle_APV(req):
 
     global actionToVary
     actionToVary = req.actionName
-    if actionToVary is not None: 
-        params['actionName'] = actionToVary
     global gripper
     gripper = req.gripper
     if gripper is not None: 
@@ -68,22 +143,23 @@ def handle_APV(req):
     execute_action(actionToVary, params)
 
 
-    # return PressButtonSrvResponse(1)
+    return APVSrvResponse(1)
 
 def execute_action(actionName, params):
     args = params.values()
-    print(args)
-    # rospy.wait_for_service('press_button_srv', timeout=60)
-    # try:
-    #     b = rospy.ServiceProxy('press_button_srv', PressButtonSrv)
-    #     resp = b('left', 'block')
-    #     print(resp.success_bool)
-    # except rospy.ServiceException, e:
-    #     print("Service call failed: %s"%e)
+    rospy.wait_for_service(KB.getService(actionName), timeout=60)
+    try:
+        b = rospy.ServiceProxy(KB.getService(actionName), KB.getSrv(actionName))
+
+        resp = b(args)
+
+        print(resp.success_bool)
+    except rospy.ServiceException, e:
+        print("Service call failed: %s"%e)
 
 def main():
     rospy.init_node("APV_node")
-    rospy.wait_for_message("/robot/sim/started", Empty)
+    # rospy.wait_for_message("/robot/sim/started", Empty)
 
     # rospy.Subscriber("left_button_pose", PoseStamped, getPoseButtonLeft)
     # rospy.Subscriber("right_button_pose", PoseStamped, getPoseButtonRight)
--- agent/scripts/knowledge_base.py
+++ agent/scripts/knowledge_base.py
@@ -22,28 +22,55 @@ from std_msgs.msg import (
 from action_primitive_variation.srv import *
 
 class KnowledgeBase(object):
-    def __init__(self, actionPrimitiveNames, actionPrimitiveServers, actionPrimitiveSrvFiles):
+    def __init__(self, actionPrimitiveNames, actionPrimitiveServices, actionPrimitiveSrvFiles):
         self.names = actionPrimitiveNames
-        self.servers = actionPrimitiveServers
+        self.services = actionPrimitiveServices
         self.srvs = actionPrimitiveSrvFiles
-        list_data = {}
+        list_data = []
+        dict_data = {}
         for i in range(len(self.names)):
-            list_data[self.names[i]] = {}
-            list_data[self.names[i]]['server'] = self.servers[i]
-            list_data[self.names[i]]['srv'] = self.srvs[i]    
+            list_data_point = {}
+            list_data_point['name'] = self.names[i]
+            list_data_point['service'] = self.services[i]
+            list_data_point['srv'] = self.srvs[i]
+            list_data.append(list_data_point)
+            dict_data[self.names[i]] = {}
+            dict_data[self.names[i]]['service'] = self.services[i]
+            dict_data[self.names[i]]['srv'] = self.srvs[i]    
         self.KB_list = list_data
-
-        self.KB_dict = 
+        self.KB_dict = dict_data
 
     def getNames(self):
         return self.names
 
-    def getServers(self):
-        return self.servers
+    def getServices(self):
+        return self.services
 
     def getSrvs(self):
         return self.srvs
 
-    def getAPData():
+    def getService(self, actionName):
+        return self.services[self.names.index(actionName)]
 
-        return 
+    def getSrv(self):
+        return self.srvs[self.names.index(actionName)]
+
+    def getAPData_list(self):
+        return self.KB_list
+
+    def getAPData_dict(self):
+        return self.KB_dict
+
+    def addAction(self, actionName, service, srv):
+        self.names.append(actionName)
+        self.services.append(service)
+        self.srvs.append(srv)
+        dict_data = {}
+        list_data = {}
+        list_data['name'] = actionName
+        list_data['service'] = service
+        list_data['srv'] = srv
+        dict_data['service'] = service
+        dict_data['srv'] = srv
+        self.KB_list.append(list_data)
+        self.KB_dict[actionName] = dict_data
