5325f53d19be0ce1cbc5e34c3733147b5fc07033
Evana <Evana13G@gmail.com>
2018-11-16 16:09:26 -0500

    building out APV, added an execute action function

--- action_primitive_variation/scripts/APV_server.py
+++ action_primitive_variation/scripts/APV_server.py
@@ -37,32 +37,59 @@ from action_primitive_variation.srv import *
 
 
 actionToVary = None 
+gripper = None
+obj = None
+button = None
+actionPrimitiveKnowledgeBase = {}
+
 
 def handle_APV(req):
-    print("Received:")
-    print("Action to Vary: ")
+    print("(Received) Action to Vary: ")
     print(req.actionName)
-    
-    
+    params = {}
+
     global actionToVary
-    actionToVary = req.actionToVary
+    actionToVary = req.actionName
+    if actionToVary is not None: 
+        params['actionName'] = actionToVary
+    global gripper
+    gripper = req.gripper
+    if gripper is not None: 
+        params['gripper'] = gripper
+    global obj
+    obj = req.obj
+    if obj is not None: 
+        params['obj'] = obj
+    global button
+    button = req.button
+    if button is not None: 
+        params['button'] = button
 
-
-
+    execute_action(actionToVary, params)
 
 
     # return PressButtonSrvResponse(1)
 
+def execute_action(actionName, params):
+    args = params.values()
+    print(args)
+    # rospy.wait_for_service('press_button_srv', timeout=60)
+    # try:
+    #     b = rospy.ServiceProxy('press_button_srv', PressButtonSrv)
+    #     resp = b('left', 'block')
+    #     print(resp.success_bool)
+    # except rospy.ServiceException, e:
+    #     print("Service call failed: %s"%e)
 
 def main():
-    rospy.init_node("press_button_node")
-    rospy.on_shutdown(delete_gazebo_models)
+    rospy.init_node("APV_node")
     rospy.wait_for_message("/robot/sim/started", Empty)
-    rospy.Subscriber("left_button_pose", PoseStamped, getPoseButtonLeft)
-    rospy.Subscriber("right_button_pose", PoseStamped, getPoseButtonRight)
-    rospy.Subscriber("block_pose", PoseStamped, getPoseBlock)
+
+    # rospy.Subscriber("left_button_pose", PoseStamped, getPoseButtonLeft)
+    # rospy.Subscriber("right_button_pose", PoseStamped, getPoseButtonRight)
+    # rospy.Subscriber("block_pose", PoseStamped, getPoseBlock)
     
-    s = rospy.Service("APV_srv", PressButtonSrv, handle_APV)
+    s = rospy.Service("APV_srv", APVSrv, handle_APV)
     rospy.spin()
     
     return 0
--- action_primitive_variation/scripts/testServer.py
+++ action_primitive_variation/scripts/testServer.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 
-from action_primitive_variation.srv import PressButtonSrv, CloseGripperSrv, OpenGripperSrv, ObtainObjectSrv
+from action_primitive_variation.srv import PressButtonSrv, CloseGripperSrv, OpenGripperSrv, ObtainObjectSrv, APVSrv
 import rospy
 
 from geometry_msgs.msg import (
@@ -16,12 +16,10 @@ from std_msgs.msg import (
 
 def main():
     rospy.init_node("test_node")
-    print("Inside testServer.py")
-    rospy.wait_for_service('press_button_srv', timeout=60)
-    print("Ready to call service")
+    rospy.wait_for_service('APV_srv', timeout=60)
     try:
-        b = rospy.ServiceProxy('press_button_srv', PressButtonSrv)
-        resp = b('left', 'block')
+        b = rospy.ServiceProxy('APV_srv', APVSrv)
+        resp = b('obtain_object', 'left', 'block', None)
         print(resp.success_bool)
     except rospy.ServiceException, e:
         print("Service call failed: %s"%e)
