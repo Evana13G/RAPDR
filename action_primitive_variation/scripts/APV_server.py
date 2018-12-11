91b297e7dac620e86f548bc2cc57ffae46753240
Evana <Evana13G@gmail.com>
2018-12-10 19:26:59 -0500

    brb

--- action_primitive_variation/scripts/APV_server.py
+++ action_primitive_variation/scripts/APV_server.py
@@ -12,6 +12,7 @@ import rospy
 import rospkg
 import rosbag
 import matplotlib.pyplot as plt
+import matplotlib.figure
 
 from gazebo_msgs.srv import (
     SpawnModel,
@@ -33,18 +34,21 @@ from baxter_core_msgs.srv import (
 )
 
 from tf.transformations import *
-
 import baxter_interface
-
 from util.knowledge_base import KnowledgeBase
-
 from action_primitive_variation.srv import *
 
 actionToVary = None 
 gripper = None
 obj = None
 button = None
-bag = rosbag.Bag('APV.bag' ,'w')
+
+leftButton_bag = rosbag.Bag('leftButton.bag' ,'w')
+rightButton_bag = rosbag.Bag('rightButton.bag' ,'w')
+block_bag = rosbag.Bag('block.bag' ,'w')
+leftGripper_bag = rosbag.Bag('leftGripper.bag' ,'w')
+rightGripper_bag = rosbag.Bag('rightGripper.bag' ,'w')
+
 BlockPose = None
 shouldRecord = False
 
@@ -54,63 +58,123 @@ AP_srvs = [PressButtonSrv, ObtainObjectSrv]
 KB = KnowledgeBase(AP_names, AP_services, AP_srvs)
 
 def handle_APV(req):
-
-    params = []
-    global actionToVary
-    actionToVary = req.actionName
     global gripper
+    global actionToVary
+    global obj
+    global button
+    global shouldRecord
+    
+    params = []
+    actionToVary = req.actionName
     gripper = req.gripper
     if gripper is not '': 
         params.append(gripper)
-    global obj
     obj = req.obj
     if obj is not '': 
         params.append(obj)
-    global button
     button = req.button
     if button is not '': 
         params.append(button)
-
-    global shouldRecord
     shouldRecord = True
     execute_action(actionToVary, params)
     shouldRecord = False
-    
+    # closeBags()
+    visualize_ROSbag_data()
     
     return APVSrvResponse(1)
 
 
 ############### START: Call back functions that check to see if ROSbag should be being recorded
-def handle_PoseBlock(data):
+def handle_block(data):
     global shouldRecord
     if shouldRecord is not False:
-        write_to_ROSbag('block_pose', data)
+        try:
+            block_bag.write('block_pose', data)
+        finally:
+            pass
+
+def handle_buttonLeft(data):
+    global shouldRecord
+    if shouldRecord is not False:
+        try:
+            block_bag.write('left_button_pose', data)
+        finally:
+            pass
+
+def handle_buttonRight(data):
+    global shouldRecord
+    if shouldRecord is not False:
+        try:
+            block_bag.write('right_button_pose', data)
+        finally:
+            pass
+
+def handle_gripperLeft(data):
+    global shouldRecord
+    if shouldRecord is not False:
+        try:
+            block_bag.write('left_gripper_pose', data)
+        finally:
+            pass
+
+def handle_gripperRight(data):
+    global shouldRecord
+    if shouldRecord is not False:
+        try:
+            block_bag.write('right_gripper_pose', data)
+        finally:
+            pass
 
 
 ############### END: Call back functions that check to see if ROSbag should be being recorded
 
 ############### START: ROSbag handling
-def write_to_ROSbag(topicName, data):
-    global bag
-    try:
-        # data = str(data.pose.position.x)
-        bag.write(topicName, data)
-    finally:
-        pass
-        # bag.close()
+def visualize_ROSbag_data():
 
-def visualize_ROSbag():
-    global bag 
-    elevData = []
-    for topic, msg, t in bag.read_messages(topics=['block_pose']):
-        elevData.append(msg.pose.position.z)
-    bag.close()
-    plt.plot(elevData)
-    plt.show()
+    global leftButton_bag 
+    global rightButton_bag 
+    global block_bag 
+    global leftGripper_bag 
+    global rightGripper_bag 
+
+    elevData_x = []
+    elevData_y = []
+    elevData_z = []
+
+    for topic, msg, t in block_bag.read_messages(topics=['block_pose']):
+        elevData_x.append(msg.pose.position.x)
+        elevData_y.append(msg.pose.position.y)
+        elevData_z.append(msg.pose.position.z)
+    block_bag.close()
+
+
+    # figure
+    # plt.plot(elevData_x)
+    # plt.show()
+    # figure
+    # plt.plot(elevData_y)
+    # plt.show()
+    # figure
+    # plt.plot(elevData_z)
+    # plt.show()
+
+
 
 #     header = { 'topic' : topic, 'type' : msg.__class__._type, 'md5sum' : msg.__class__._md5sum, 'message_definition' : msg._full_text }
 # AttributeError: type object 'str' has no attribute '_type'
 
+def closeBags():
+    global leftButton_bag 
+    global rightButton_bag 
+    global block_bag 
+    global leftGripper_bag 
+    global rightGripper_bag 
+
+    leftButton_bag.close() 
+    rightButton_bag.close() 
+    block_bag.close() 
+    leftGripper_bag.close() 
+    rightGripper_bag.close() 
 ############### END: ROSbag handling
 
 
@@ -136,10 +200,11 @@ def main():
     rospy.init_node("APV_node")
     rospy.wait_for_message("/robot/sim/started", Empty)
 
-    rospy.Subscriber("left_button_pose", PoseStamped, handle_PoseButtonLeft)
-    rospy.Subscriber("right_button_pose", PoseStamped, handle_PoseButtonRight)    
-    rospy.Subscriber("block_pose", PoseStamped, handle_PoseBlock)
-
+    rospy.Subscriber("left_button_pose", PoseStamped, handle_buttonLeft)
+    rospy.Subscriber("right_button_pose", PoseStamped, handle_buttonRight)    
+    rospy.Subscriber("block_pose", PoseStamped, handle_block)
+    rospy.Subscriber("left_gripper_pose", Point, handle_gripperLeft)
+    rospy.Subscriber("right_gripper_pose", Point, handle_gripperRight)
 
     s = rospy.Service("APV_srv", APVSrv, handle_APV)
     rospy.spin()
--- scenario_data/scripts/scenario_data.py
+++ scenario_data/scripts/scenario_data.py
@@ -43,106 +43,118 @@ from action_primitive_variation.srv import *
 LeftButtonPose = None
 RightButtonPose = None
 BlockPose = None
-#Left Gripper Left Finger Pose
-lglfPose = None
-#Left Gripper Right Finger Pose
-lgrfPose = None
-#Right Gripper Left Finger Pose
-rglfPose = None
-#Right Gripper Right Finger Pose
-rgrfPose = None
-#Left Gripper Pose
-leftGripperPose = None
-#Right Gripper Pose
+LeftGripperPose = None
 rightGripperPose = None
 
-def getPoseButtonLeft(data):
+# #Left Gripper Left Finger Pose
+# lglfPose = None
+# #Left Gripper Right Finger Pose
+# lgrfPose = None
+# #Right Gripper Left Finger Pose
+# rglfPose = None
+# #Right Gripper Right Finger Pose
+# rgrfPose = None
+# #Left Gripper Pose
+# leftGripperPose = None
+# #Right Gripper Pose
+# rightGripperPose = None
+
+def setPoseButtonLeft(data):
     global LeftButtonPose
     LeftButtonPose = data
 
-def getPoseButtonRight(data):
+def setPoseButtonRight(data):
     global RightButtonPose
     RightButtonPose = data
 
-def getPoseBlock(data):
+def setPoseBlock(data):
     global BlockPose
     BlockPose = data
 
+def setPoseGripperLeft(data):
+    global LeftGripperPose
+    LeftGripperPose = data
 
+def setPoseGripperRight(data):
+    global RightGripperPose
+    RightGripperPose = data
 
-def getLeftGripper():
-    rospy.wait_for_service('/gazebo/get_link_state')
+# def getLeftGripper():
+#     rospy.wait_for_service('/gazebo/get_link_state')
     
-    try:
-        lglf_link_state = rospy.ServiceProxy('/gazebo/get_link_state', GetLinkState)
-        resp_lglf_link_state = lglf_link_state('l_gripper_l_finger', 'world')
-        global lglfPose
-        lglfPose = resp_lglf_link_state.link_state.pose.position
-    except rospy.ServiceException, e:
-        rospy.logerr("get_link_state for l_gripper_l_finger: {0}".format(e))
-    print("LGLF pose: ")
-    print(lglfPose)
+#     try:
+#         lglf_link_state = rospy.ServiceProxy('/gazebo/get_link_state', GetLinkState)
+#         resp_lglf_link_state = lglf_link_state('l_gripper_l_finger', 'world')
+#         global lglfPose
+#         lglfPose = resp_lglf_link_state.link_state.pose.position
+#     except rospy.ServiceException, e:
+#         rospy.logerr("get_link_state for l_gripper_l_finger: {0}".format(e))
+#     # print("LGLF pose: ")
+#     # print(lglfPose)
 
-    try:
-        lgrf_link_state = rospy.ServiceProxy('/gazebo/get_link_state', GetLinkState)
-        resp_lgrf_link_state = lgrf_link_state('l_gripper_r_finger', 'world')
-        global lgrfPose
-        lgrfPose = resp_lgrf_link_state.link_state.pose.position
-    except rospy.ServiceException, e:
-        rospy.logerr("get_link_state for l_gripper_r_finger: {0}".format(e))
-    print("LGRF pose: ")
-    print(lgrfPose)
+#     try:
+#         lgrf_link_state = rospy.ServiceProxy('/gazebo/get_link_state', GetLinkState)
+#         resp_lgrf_link_state = lgrf_link_state('l_gripper_r_finger', 'world')
+#         global lgrfPose
+#         lgrfPose = resp_lgrf_link_state.link_state.pose.position
+#     except rospy.ServiceException, e:
+#         rospy.logerr("get_link_state for l_gripper_r_finger: {0}".format(e))
+#     # print("LGRF pose: ")
+#     # print(lgrfPose)
 
-    global leftGripperPose
-    leftGripperPose = Point()
-    leftGripperPose.x = (lglfPose.x + lgrfPose.x)/2
-    leftGripperPose.y = (lglfPose.y + lgrfPose.y)/2
-    leftGripperPose.z = (lglfPose.z + lgrfPose.z)/2
+#     global leftGripperPose
+#     leftGripperPose = Point()
+#     leftGripperPose.x = (lglfPose.x + lgrfPose.x)/2
+#     leftGripperPose.y = (lglfPose.y + lgrfPose.y)/2
+#     leftGripperPose.z = (lglfPose.z + lgrfPose.z)/2
 
-    print("Left Gripper Pose: ")
-    print(leftGripperPose)
+#     print("Left Gripper Pose: ")
+#     print(leftGripperPose)
 
-def getRightGripper():
-    rospy.wait_for_service('/gazebo/get_link_state')
+# def getRightGripper():
+#     rospy.wait_for_service('/gazebo/get_link_state')
     
-    try:
-        rglf_link_state = rospy.ServiceProxy('/gazebo/get_link_state', GetLinkState)
-        resp_rglf_link_state = rglf_link_state('r_gripper_l_finger', 'world')
-        global rglfPose
-        rglfPose = resp_rglf_link_state.link_state.pose.position
-    except rospy.ServiceException, e:
-        rospy.logerr("get_link_state for r_gripper_l_finger: {0}".format(e))
-    print("RGLF pose: ")
-    print(rglfPose)
+#     try:
+#         rglf_link_state = rospy.ServiceProxy('/gazebo/get_link_state', GetLinkState)
+#         resp_rglf_link_state = rglf_link_state('r_gripper_l_finger', 'world')
+#         global rglfPose
+#         rglfPose = resp_rglf_link_state.link_state.pose.position
+#     except rospy.ServiceException, e:
+#         rospy.logerr("get_link_state for r_gripper_l_finger: {0}".format(e))
+#     # print("RGLF pose: ")
+#     # print(rglfPose)
 
-    try:
-        rgrf_link_state = rospy.ServiceProxy('/gazebo/get_link_state', GetLinkState)
-        resp_rgrf_link_state = rgrf_link_state('r_gripper_r_finger', 'world')
-        global lgrfPose
-        rgrfPose = resp_rgrf_link_state.link_state.pose.position
-    except rospy.ServiceException, e:
-        rospy.logerr("get_link_state for r_gripper_r_finger: {0}".format(e))
-    print("RGRF pose: ")
-    print(rgrfPose)
+#     try:
+#         rgrf_link_state = rospy.ServiceProxy('/gazebo/get_link_state', GetLinkState)
+#         resp_rgrf_link_state = rgrf_link_state('r_gripper_r_finger', 'world')
+#         global lgrfPose
+#         rgrfPose = resp_rgrf_link_state.link_state.pose.position
+#     except rospy.ServiceException, e:
+#         rospy.logerr("get_link_state for r_gripper_r_finger: {0}".format(e))
+#     # print("RGRF pose: ")
+#     # print(rgrfPose)
 
-    global rightGripperPose
-    rightGripperPose = Point()
-    rightGripperPose.x = (rglfPose.x + rgrfPose.x)/2
-    rightGripperPose.y = (rglfPose.y + rgrfPose.y)/2
-    rightGripperPose.z = (rglfPose.z + rgrfPose.z)/2
+#     global rightGripperPose
+#     rightGripperPose = Point()
+#     rightGripperPose.x = (rglfPose.x + rgrfPose.x)/2
+#     rightGripperPose.y = (rglfPose.y + rgrfPose.y)/2
+#     rightGripperPose.z = (rglfPose.z + rgrfPose.z)/2
 
-    print("Right Gripper Pose: ")
-    print(rightGripperPose)
+
+#     print("Right Gripper Pose: ")
+#     print(rightGripperPose)
 
 
 
 def main():
     rospy.init_node("scenario_data_node")
     rospy.wait_for_message("/robot/sim/started", Empty)
-    
-    rospy.Subscriber("left_button_pose", PoseStamped, getPoseButtonLeft)
-    rospy.Subscriber("right_button_pose", PoseStamped, getPoseButtonRight)
-    rospy.Subscriber("block_pose", PoseStamped, getPoseBlock)
+   
+    rospy.Subscriber("left_gripper_pose", Point, setPoseGripperLeft)
+    rospy.Subscriber("right_gripper_pose", Point, setPoseGripperRight)
+    rospy.Subscriber("left_button_pose", PoseStamped, setPoseButtonLeft)
+    rospy.Subscriber("right_button_pose", PoseStamped, setPoseButtonRight)
+    rospy.Subscriber("block_pose", PoseStamped, setPoseBlock)
 
     # rospy.wait_for_service('/gazebo/get_link_state')
     
@@ -161,10 +173,9 @@ def main():
     #     # pub_cafe_table_pose.publish(poseStamped_cafe_table)
     # except rospy.ServiceException, e:
     #     rospy.logerr("get_link_state for l_gripper_l_finger: {0}".format(e))
-
-    # global lglfPose
-    getLeftGripper()
-    getRightGripper()
+    # # global lglfPose
+    # getLeftGripper()
+    # getRightGripper()
             
     
 
