c7d2359a59538bf4960be0f8f994f064ae870279
Evana <Evana13G@gmail.com>
2018-12-11 17:33:08 -0500

    continued changes to APV

--- action_primitive_variation/scripts/APV_server.py
+++ action_primitive_variation/scripts/APV_server.py
@@ -43,9 +43,9 @@ gripper = None
 obj = None
 button = None
 
+block_bag = rosbag.Bag('block.bag' ,'w')
 leftButton_bag = rosbag.Bag('leftButton.bag' ,'w')
 rightButton_bag = rosbag.Bag('rightButton.bag' ,'w')
-block_bag = rosbag.Bag('block.bag' ,'w')
 leftGripper_bag = rosbag.Bag('leftGripper.bag' ,'w')
 rightGripper_bag = rosbag.Bag('rightGripper.bag' ,'w')
 
@@ -75,16 +75,42 @@ def handle_APV(req):
     button = req.button
     if button is not '': 
         params.append(button)
+
+    # open the bags in case they were closed
+    global leftButton_bag 
+    global rightButton_bag 
+    global block_bag 
+    global leftGripper_bag 
+    global rightGripper_bag
+
+    block_bag = rosbag.Bag('block.bag' ,'w')
+    leftButton_bag = rosbag.Bag('leftButton.bag' ,'w')
+    rightButton_bag = rosbag.Bag('rightButton.bag' ,'w')
+    leftGripper_bag = rosbag.Bag('leftGripper.bag' ,'w')
+    rightGripper_bag = rosbag.Bag('rightGripper.bag' ,'w')
+    jointState_bag = rosbag.Bag('jointState.bag' ,'w')
+
     shouldRecord = True
     execute_action(actionToVary, params)
     shouldRecord = False
-    # closeBags()
+  
     visualize_ROSbag_data()
-    
+
+    closeBags()
+
     return APVSrvResponse(1)
 
 
 ############### START: Call back functions that check to see if ROSbag should be being recorded
+def handle_jointStates(data):
+    global shouldRecord
+    if shouldRecord is not False:
+        try:
+            jointState_bag.write('robot/joint_states', data)
+        finally:
+            pass
+
+
 def handle_block(data):
     global shouldRecord
     if shouldRecord is not False:
@@ -97,7 +123,7 @@ def handle_buttonLeft(data):
     global shouldRecord
     if shouldRecord is not False:
         try:
-            block_bag.write('left_button_pose', data)
+            leftButton_bag.write('left_button_pose', data)
         finally:
             pass
 
@@ -105,7 +131,7 @@ def handle_buttonRight(data):
     global shouldRecord
     if shouldRecord is not False:
         try:
-            block_bag.write('right_button_pose', data)
+            rightButton_bag.write('right_button_pose', data)
         finally:
             pass
 
@@ -113,7 +139,7 @@ def handle_gripperLeft(data):
     global shouldRecord
     if shouldRecord is not False:
         try:
-            block_bag.write('left_gripper_pose', data)
+            leftGripper_bag.write('left_gripper_pose', data)
         finally:
             pass
 
@@ -121,7 +147,7 @@ def handle_gripperRight(data):
     global shouldRecord
     if shouldRecord is not False:
         try:
-            block_bag.write('right_gripper_pose', data)
+            rightGripper_bag.write('right_gripper_pose', data)
         finally:
             pass
 
@@ -131,37 +157,103 @@ def handle_gripperRight(data):
 ############### START: ROSbag handling
 def visualize_ROSbag_data():
 
+    print("Visualizing the bag");
+
     global leftButton_bag 
     global rightButton_bag 
     global block_bag 
     global leftGripper_bag 
     global rightGripper_bag 
+    global jointState_bag
 
-    elevData_x = []
-    elevData_y = []
-    elevData_z = []
 
+    """
+    elevData_x_block = []
+    elevData_y_block = []
+    elevData_z_block = []
+    c = 0
     for topic, msg, t in block_bag.read_messages(topics=['block_pose']):
-        elevData_x.append(msg.pose.position.x)
-        elevData_y.append(msg.pose.position.y)
-        elevData_z.append(msg.pose.position.z)
-    block_bag.close()
+        #if t < last_time:
+        #    break
+        #else:
+        #    t = last_time
+        elevData_x_block.append(msg.pose.position.x)
+        elevData_y_block.append(msg.pose.position.y)
+        elevData_z_block.append(msg.pose.position.z)
+        # c+=1
+
+    print("Added "+str(c)+" messages to plot")
+
+    elevData_x_lbutton = []
+    elevData_y_lbutton = []
+    elevData_z_lbutton = []
+    for topic, msg, t in leftButton_bag.read_messages(topics=['left_button_pose']):
+        elevData_x_lbutton.append(msg.pose.position.x)
+        elevData_y_lbutton.append(msg.pose.position.y)
+        elevData_z_lbutton.append(msg.pose.position.z)
+
+    elevData_x_rbutton = []
+    elevData_y_rbutton = []
+    elevData_z_rbutton = []
+    for topic, msg, t in rightButton_bag.read_messages(topics=['right_button_pose']):
+        elevData_x_rbutton.append(msg.pose.position.x)
+        elevData_y_rbutton.append(msg.pose.position.y)
+        elevData_z_rbutton.append(msg.pose.position.z)
+
+    elevData_x_lgripper = []
+    elevData_y_lgripper = []
+    elevData_z_lgripper = []
+    for topic, msg, t in leftGripper_bag.read_messages(topics=['left_gripper_pose']):
+        elevData_x_lgripper.append(msg.x)
+        elevData_y_lgripper.append(msg.y)
+        elevData_z_lgripper.append(msg.z)
+
+    elevData_x_rgripper = []
+    elevData_y_rgripper = []
+    elevData_z_rgripper = []
+    for topic, msg, t in rightGripper_bag.read_messages(topics=['right_gripper_pose']):
+        elevData_x_rgripper.append(msg.x)
+        elevData_y_rgripper.append(msg.y)
+        elevData_z_rgripper.append(msg.z)
+
+    plt.plot(elevData_x_block, 'r--', elevData_y_block, 'r1', elevData_z_block, 'r^',
+             elevData_x_lbutton, 'b--', elevData_y_lbutton, 'b1', elevData_z_lbutton, 'b^',
+             elevData_x_rbutton, 'g--', elevData_y_rbutton, 'g1', elevData_z_rbutton, 'g^',
+             elevData_x_lgripper, 'k--', elevData_y_lgripper, 'k1', elevData_z_lgripper, 'k^',
+             elevData_x_rgripper, 'y--', elevData_y_rgripper, 'y1', elevData_z_rgripper, 'y^',)
+    """
 
 
-    # figure
-    # plt.plot(elevData_x)
-    # plt.show()
-    # figure
-    # plt.plot(elevData_y)
-    # plt.show()
-    # figure
-    # plt.plot(elevData_z)
-    # plt.show()
+    x = []
+    y = []
+    z = []
+'''
+header: 
+  seq: 422998
+  stamp: 
+    secs: 8460
+    nsecs: 369000000
+  frame_id: ''
+name: [head_pan, l_gripper_l_finger_joint, l_gripper_r_finger_joint, left_e0, left_e1, left_s0,
+  left_s1, left_w0, left_w1, left_w2, r_gripper_l_finger_joint, r_gripper_r_finger_joint,
+  right_e0, right_e1, right_s0, right_s1, right_w0, right_w1, right_w2]
+position: [0.00037950190107327586, -7.320651061985309e-10, -9.007194895622202e-06, -1.1909722946993613, 1.9382078592691734, -0.07959292224814174, -0.9997521277253458, 0.6696652976179029, 1.0318677316390072, -0.4913284744172408, -3.0167292263687043e-09, -0.020833001482205276, 0.01544288531582172, 0.4941136969391611, -0.27231906855958954, 1.0470000130417105, 0.21297591992677134, 0.02276380988889848, 0.1320914918373033]
+velocity: [-1.3409318717564828e-08, 1.5172544598044918e-07, 8.686871031266307e-07, 2.872689291693379e-07, -3.533709074745e-07, -2.2422995970956474e-07, -2.6765843977145595e-08, -7.418683112386929e-07, 4.974558504249287e-07, 1.2290406496279368e-06, -1.5094280632432012e-07, -1.5087212242602784e-07, -1.37584760108946e-06, -2.1939020649849428e-07, -8.306468466396253e-08, 3.676123786542591e-10, 2.6048955182016797e-05, -8.427936596269117e-07, 2.4698018362627136e-05]
+effort: [0.0, 9.124296028588603e-07, 0.009008088544217203, -0.12429816348236145, -0.16163667914392832, 5.567546867979445e-06, -0.15937851568281758, 0.008033237475864041, -0.004958976126072656, 3.9155898079457074e-06, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0]
+
+'''
+
+    for topic, msg, t in leftButton_bag.read_messages(topics=['robot/joint_states']):
+        x.append(msg.pose.position.x)
+        y.append(msg.pose.position.y)
+        z.append(msg.pose.position.z)
 
 
 
-#     header = { 'topic' : topic, 'type' : msg.__class__._type, 'md5sum' : msg.__class__._md5sum, 'message_definition' : msg._full_text }
-# AttributeError: type object 'str' has no attribute '_type'
+
+    print(len(x))
+    plt.plot(x, 'r--', y, 'r1', z, 'r^')
+    plt.show()
 
 def closeBags():
     global leftButton_bag 
@@ -205,6 +297,8 @@ def main():
     rospy.Subscriber("block_pose", PoseStamped, handle_block)
     rospy.Subscriber("left_gripper_pose", Point, handle_gripperLeft)
     rospy.Subscriber("right_gripper_pose", Point, handle_gripperRight)
+
+    rospy.Subscriber("robot/joint_states", Point, handle_jointStates)
 
     s = rospy.Service("APV_srv", APVSrv, handle_APV)
     rospy.spin()
