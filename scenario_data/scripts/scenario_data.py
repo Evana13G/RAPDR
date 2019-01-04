51997ee44365c231b6acf95b5808db442cee0ae1
mateoguaman <mateo.guaman1998@gmail.com>
2019-01-03 20:12:38 -0500

    got first iteration of vision system working

--- scenario_data/scripts/scenario_data.py
+++ scenario_data/scripts/scenario_data.py
@@ -1,5 +1,15 @@
 #!/usr/bin/env python
 
+
+from __future__ import print_function, division
+
+import roslib
+#roslib.load_manifest('my_package')
+import sys
+import cv2
+import math
+import time
+from cv_bridge import CvBridge, CvBridgeError
 import argparse
 import struct
 import sys
@@ -17,9 +27,14 @@ from gazebo_msgs.srv import (
 from gazebo_msgs.msg import (
     LinkState,
 )
+from sensor_msgs.msg import (
+    Image,
+)
 from geometry_msgs.msg import (
     PoseStamped,
     Pose,
+    PoseArray,
+    PoseWithCovarianceStamped,
     Point,
     Quaternion,
 )
@@ -46,19 +61,6 @@ BlockPose = None
 LeftGripperPose = None
 rightGripperPose = None
 
-# #Left Gripper Left Finger Pose
-# lglfPose = None
-# #Left Gripper Right Finger Pose
-# lgrfPose = None
-# #Right Gripper Left Finger Pose
-# rglfPose = None
-# #Right Gripper Right Finger Pose
-# rgrfPose = None
-# #Left Gripper Pose
-# leftGripperPose = None
-# #Right Gripper Pose
-# rightGripperPose = None
-
 def setPoseButtonLeft(data):
     global LeftButtonPose
     LeftButtonPose = data
@@ -79,105 +81,62 @@ def setPoseGripperRight(data):
     global RightGripperPose
     RightGripperPose = data
 
-# def getLeftGripper():
-#     rospy.wait_for_service('/gazebo/get_link_state')
-    
-#     try:
-#         lglf_link_state = rospy.ServiceProxy('/gazebo/get_link_state', GetLinkState)
-#         resp_lglf_link_state = lglf_link_state('l_gripper_l_finger', 'world')
-#         global lglfPose
-#         lglfPose = resp_lglf_link_state.link_state.pose.position
-#     except rospy.ServiceException, e:
-#         rospy.logerr("get_link_state for l_gripper_l_finger: {0}".format(e))
-#     # print("LGLF pose: ")
-#     # print(lglfPose)
 
-#     try:
-#         lgrf_link_state = rospy.ServiceProxy('/gazebo/get_link_state', GetLinkState)
-#         resp_lgrf_link_state = lgrf_link_state('l_gripper_r_finger', 'world')
-#         global lgrfPose
-#         lgrfPose = resp_lgrf_link_state.link_state.pose.position
-#     except rospy.ServiceException, e:
-#         rospy.logerr("get_link_state for l_gripper_r_finger: {0}".format(e))
-#     # print("LGRF pose: ")
-#     # print(lgrfPose)
+class image_converter:
+    def __init__(self):
+        print("Inside Constructor. Pubs and subs set up")
+        self.bridge = CvBridge()
+        self.initTime = 0
+        self.savedFrames = {}
+        self.savedFramesStr = ""
+        self.image_sub = rospy.Subscriber("/cameras/head_camera/image", Image, self.callbackImage)
 
-#     global leftGripperPose
-#     leftGripperPose = Point()
-#     leftGripperPose.x = (lglfPose.x + lgrfPose.x)/2
-#     leftGripperPose.y = (lglfPose.y + lgrfPose.y)/2
-#     leftGripperPose.z = (lglfPose.z + lgrfPose.z)/2
-
-#     print("Left Gripper Pose: ")
-#     print(leftGripperPose)
-
-# def getRightGripper():
-#     rospy.wait_for_service('/gazebo/get_link_state')
-    
-#     try:
-#         rglf_link_state = rospy.ServiceProxy('/gazebo/get_link_state', GetLinkState)
-#         resp_rglf_link_state = rglf_link_state('r_gripper_l_finger', 'world')
-#         global rglfPose
-#         rglfPose = resp_rglf_link_state.link_state.pose.position
-#     except rospy.ServiceException, e:
-#         rospy.logerr("get_link_state for r_gripper_l_finger: {0}".format(e))
-#     # print("RGLF pose: ")
-#     # print(rglfPose)
-
-#     try:
-#         rgrf_link_state = rospy.ServiceProxy('/gazebo/get_link_state', GetLinkState)
-#         resp_rgrf_link_state = rgrf_link_state('r_gripper_r_finger', 'world')
-#         global lgrfPose
-#         rgrfPose = resp_rgrf_link_state.link_state.pose.position
-#     except rospy.ServiceException, e:
-#         rospy.logerr("get_link_state for r_gripper_r_finger: {0}".format(e))
-#     # print("RGRF pose: ")
-#     # print(rgrfPose)
-
-#     global rightGripperPose
-#     rightGripperPose = Point()
-#     rightGripperPose.x = (rglfPose.x + rgrfPose.x)/2
-#     rightGripperPose.y = (rglfPose.y + rgrfPose.y)/2
-#     rightGripperPose.z = (rglfPose.z + rgrfPose.z)/2
-
-
-#     print("Right Gripper Pose: ")
-#     print(rightGripperPose)
-
+    def callbackImage(self, data):
+        try:
+            cv_image = self.bridge.imgmsg_to_cv2(data, "bgr8")
+            print("Successful Conversion")
+        except CvBridgeError as e:
+            print(e)
+        frame = cv_image
+        #cv2.imshow('frame', frame)
+#        frame = cv2.resize(frame,None,fx=0.25, fy=0.25, interpolation = cv2.INTER_CUBIC)
+#        frameCopy = frame.copy()
+#        # Convert BGR to HSV
+        hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+        lower_green = np.array([40,40,40])
+        upper_green = np.array([200,255,255])
+#        lower_red = np.array([0,0,255])
+#        upper_red = np.array([150,150, 255])
+#
+#        # Threshold the HSV image to get only blue colors
+        mask = cv2.inRange(hsv, lower_green, upper_green)
+        print(np.count_nonzero(mask))
+#        contours,_ = cv2.findContours(mask,cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_SIMPLE)
+#        for cnt in contours:
+#            approx = cv2.approxPolyDP(cnt,0.01*cv2.arcLength(cnt,True),True)
+#            #print(len(approx))
+#            if len(approx)==4:
+#                #print("square")
+#                cv2.drawContours(frameCopy,[cnt],0,(0,0,255),-1)
+#        maskRect = cv2.inRange(frameCopy, lower_red, upper_red)
+#
+#        kernel = np.ones((5,5), np.uint8)
+#        erosion = cv2.erode(maskRect, kernel, iterations = 1)
+#        erosionArray = np.asarray(erosion)
+#        areaErosion = np.count_nonzero(erosionArray)
 
 
 def main():
     rospy.init_node("scenario_data_node")
     rospy.wait_for_message("/robot/sim/started", Empty)
-   
+ 
     rospy.Subscriber("left_gripper_pose", Point, setPoseGripperLeft)
     rospy.Subscriber("right_gripper_pose", Point, setPoseGripperRight)
     rospy.Subscriber("left_button_pose", PoseStamped, setPoseButtonLeft)
     rospy.Subscriber("right_button_pose", PoseStamped, setPoseButtonRight)
     rospy.Subscriber("block_pose", PoseStamped, setPoseBlock)
 
-    # rospy.wait_for_service('/gazebo/get_link_state')
-    
-    # try:
-    #     lglf_link_state = rospy.ServiceProxy('/gazebo/get_link_state', GetLinkState)
-    #     resp_lglf_link_state = lgrf_link_state('l_gripper_l_finger', 'world')
-    #     global lglfPose
-    #     lglfPose = resp_lglf_link_state.pose
-
-    #     # cafe_table_ms = rospy.ServiceProxy('/gazebo/get_model_state', GetModelState)
-    #     # resp_cafe_table_ms = cafe_table_ms("cafe_table", "");
-    #     # pose_cafe_table = resp_cafe_table_ms.pose
-    #     # header_cafe_table = resp_cafe_table_ms.header
-    #     # header_cafe_table.frame_id = frameid_var
-    #     # poseStamped_cafe_table = PoseStamped(header=header_cafe_table, pose=pose_cafe_table)
-    #     # pub_cafe_table_pose.publish(poseStamped_cafe_table)
-    # except rospy.ServiceException, e:
-    #     rospy.logerr("get_link_state for l_gripper_l_finger: {0}".format(e))
-    # # global lglfPose
-    # getLeftGripper()
-    # getRightGripper()
-            
-    
+    ic = image_converter()
 
     rospy.spin()
     
